05ab7dd71f3f504253c81ed42c77a54cb6706f5a
mircea bardan <bardan.mircea.calin@gmail.com>
2023-05-31 03:33:33 +0200

    updated init_db

--- init_db.js
+++ init_db.js
@@ -1,8 +1,11 @@
-const {Toy, Category} = require('./models')
+const {Toy, Category, Elf, Wish, Schedule} = require('./models')
 
 const create_tables = async () => {
     await Category.sync({force : true});
     await Toy.sync({force : true});
+    await Elf.sync({force : true});
+    await Wish.sync({force : true});
+    await Schedule.sync({force : true});
 }
 
 create_tables()
